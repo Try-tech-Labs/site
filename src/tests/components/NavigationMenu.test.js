ac8392cf65b62c4e8f46d3c68649dc04dbedf68b
claytonrv <veras.clayton@gmail.com>
2020-12-31 17:34:36 -0300

    Lint correction on project, configured prettier as default formartter

--- src/tests/components/NavigationMenu.test.js
+++ src/tests/components/NavigationMenu.test.js
@@ -1,68 +1,68 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
 
-import { cleanup, render, screen, fireEvent, waitFor } from '@testing-library/react'
+import {
+  cleanup,
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react";
 
-import NavigationMenu from '../../components/NavigationMenu'
-
+import NavigationMenu from "../../components/NavigationMenu";
 
 const setup = () => {
+  const props = {
+    useLogo: true,
+  };
 
+  const wrapper = render(<NavigationMenu {...props} />);
+  return { wrapper, props };
+};
+
+describe("DeveloperCard", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("displays the navigation links", () => {
+    const { wrapper } = setup();
+    const { getByText } = wrapper;
+    getByText("Home");
+    getByText("About us");
+    getByText("Our tries");
+    getByText("Contribute");
+  });
+
+  it("not displays the site logo when on home", () => {
+    const { wrapper } = setup();
+    const { queryByTestId } = wrapper;
+    const logo = queryByTestId("logo");
+    expect(logo).toBeNull();
+  });
+
+  it("displays the site logo when out of the home", async () => {
+    const { wrapper } = setup();
+    const { getByAltText, getByText } = wrapper;
+    const about_us_button = getByText("About us");
+    fireEvent.click(about_us_button);
+    await waitFor(() => screen.findByAltText("Web site logo image"));
+    const logo = getByAltText("Web site logo image");
+    expect(logo.getAttribute("width")).toEqual("162");
+    expect(logo.getAttribute("height")).toEqual("70");
+    expect(logo.classList.contains("site_menu")).toBe(true);
+  });
+
+  it("doesnt breaks when renders without site logo", () => {
     const props = {
-        useLogo: true
-    }
-
-    const wrapper = render(
-        <NavigationMenu {...props} />
-    )
-    return {wrapper, props}
-}
-
-describe('DeveloperCard', () => {
-
-    afterEach (() => {
-        cleanup()
-    }) 
-
-    it('displays the navigation links', () => {
-        const { wrapper } = setup()
-        const { getByText } = wrapper
-        getByText("Home")
-        getByText("About us")
-        getByText("Our tries")
-        getByText("Contribute")
-
-    })
-
-    it('not displays the site logo when on home', () => {
-        const { wrapper } = setup()
-        const { queryByTestId } = wrapper
-        const logo = queryByTestId("logo")
-        expect(logo).toBeNull();
-    })
-
-    it('displays the site logo when out of the home', async () => {
-        const { wrapper } = setup()
-        const { getByAltText, getByText } = wrapper
-        const about_us_button = getByText('About us')
-        fireEvent.click(about_us_button)
-        await waitFor(() => screen.findByAltText("Web site logo image")); 
-        const logo = getByAltText("Web site logo image"); 
-        expect(logo.getAttribute('width')).toEqual("162")
-        expect(logo.getAttribute('height')).toEqual("70")
-        expect(logo.classList.contains('site_menu')).toBe(true)
-    })
-
-    it('doesnt breaks when renders without site logo', () => {
-        const props = {
-            useLogo: false
-        }
-        const wrapper = render(<NavigationMenu {...{props}} />)
-        const { getByText, queryByTestId } = wrapper
-        getByText("Home")
-        getByText("About us")
-        getByText("Our tries")
-        getByText("Contribute")
-        expect(queryByTestId('logo')).toBeNull()
-    })
-})
+      useLogo: false,
+    };
+    const wrapper = render(<NavigationMenu {...{ props }} />);
+    const { getByText, queryByTestId } = wrapper;
+    getByText("Home");
+    getByText("About us");
+    getByText("Our tries");
+    getByText("Contribute");
+    expect(queryByTestId("logo")).toBeNull();
+  });
+});
--- src/tests/pages/Contribute.test.js
+++ src/tests/pages/Contribute.test.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
 
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render } from "@testing-library/react";
 
-import Contribute from '../../pages/Contribute';
-
+import Contribute from "../../pages/Contribute";
 
 const setup = () => {
+  const wrapper = render(<Contribute />);
+  return { wrapper };
+};
 
-    const wrapper = render(
-        <Contribute />
-    )
-    return {wrapper}
-}
+describe("Contribute page", () => {
+  afterEach(() => {
+    cleanup();
+  });
 
-describe('Contribute page', () => {
-
-    afterEach (() => {
-        cleanup()
-    }) 
-
-    it('renders with correct information', () => {
-        const { wrapper } = setup()
-        const { getByText, findByText, getByAltText } = wrapper
-        getByText("Do you want to join us")
-        getByAltText("Question mark with green dot")
-        getByAltText("Scientist in front some test tubes")
-        findByText("Suggest a change on any public project or start to code and open a pull request.")
-        getByText("Access now our github page!")
-    })
-})
+  it("renders with correct information", () => {
+    const { wrapper } = setup();
+    const { getByText, findByText, getByAltText } = wrapper;
+    getByText("Do you want to join us");
+    getByAltText("Question mark with green dot");
+    getByAltText("Scientist in front some test tubes");
+    findByText(
+      "Suggest a change on any public project or start to code and open a pull request."
+    );
+    getByText("Access now our github page!");
+  });
+});
