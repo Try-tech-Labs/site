8fcd442b5c83f214a3c6a13ade9f24528d87d7eb
claytonrv <veras.clayton@gmail.com>
2020-12-31 18:37:40 -0300

    Added link to home and hover styling to the footer button

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -7,7 +7,7 @@ export default function SiteFooter(){
         <div className="footer">
             <div className="footer_content">
                 <h3 className="footer_text">Made with 💜 by TryTechLabs</h3>
-                <button className="footer_button">Back to home</button>
+                <button className="footer_button"><a href="#page-landing">Back to home</a></button>
             </div>
         </div>
     )
