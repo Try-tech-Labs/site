a7a76e4bc0482951c23579b3548d2ba54e602a56
Clayton R. Veras <veras.clayton@gmail.com>
2021-01-01 21:11:56 -0300

    Showing the current page on navigation menu (#16)

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/components/NavigationMenu.css';
 
@@ -6,26 +6,35 @@ import SiteLogo from '../components/SiteLogo';
 
 export default function NavigationMenu() {
     const [useLogo, setUseLogo] = useState(false);
+    const [currentPage, setCurrentPage] = useState('page-landing');
 
+    const isActive = (element_id:string) =>{
+        if(element_id == currentPage){
+            return "active"
+        }
+        return ""
+    }
+    
     const handlePageChange = (pageToGo: String) => {
         setTimeout(() => {
-            if (pageToGo == "page-landing"){
+            if (pageToGo == "home"){
                 setUseLogo(false)
             }else{
                 setUseLogo(true)
             }
+            setCurrentPage(pageToGo.toString())
         }, 200);         
     }
 
     return (
         <div id="navigation-menu">
-            <div className="menu-grid">
+            <div>
                 {useLogo && (<SiteLogo data-testid="logo" className="site_menu" width={162} height={70} />)}
                 <div className="menu">
-                    <a href="#page-landing" onClick={ () => handlePageChange('page-landing') }>Home</a>
-                    <a href="#page-developers" onClick={ () => handlePageChange('page-developers') }>About us</a>
-                    <a href="#page-projects" onClick={ () => handlePageChange('page-projects') }>Our tries</a>
-                    <a href="#page-contribute" onClick={ () => handlePageChange('page-contribute') }>Contribute</a>
+                    <a className={isActive("home")} href="#page-landing" onClick={ () => handlePageChange('home') }>Home</a>
+                    <a className={isActive("about_us")} href="#page-developers" onClick={ () => handlePageChange('about_us') }>About us</a>
+                    <a className={isActive("our_tries")} href="#page-projects" onClick={ () => handlePageChange('our_tries') }>Our tries</a>
+                    <a className={isActive("contribute")} href="#page-contribute" onClick={ () => handlePageChange('contribute') }>Contribute</a>
                 </div>
             </div>
         </div>
